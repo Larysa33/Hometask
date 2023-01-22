771cf81910e89ff8106c6af8dbf3f567fe1a3e01
Larysa33 <ligorev@yahoo.com>
2023-01-22 03:51:19 +0300

    Working decision for ATM project.

--- src/by/teachmeskills/hometask7/additionalTask2/AtmDemo.java
+++ src/by/teachmeskills/hometask7/additionalTask2/AtmDemo.java
@@ -1,12 +1,17 @@
 package by.teachmeskills.hometask7.additionalTask2;
 
+import java.util.Arrays;
+
 public class AtmDemo {
     public static void main(String[] args) {
-        Atm atm = new Atm(0, 20, 100);
+        Atm atm = new Atm(0, 0, 0);
         atm.atmInfo();
 
-        atm.topUp(20, 40, 100);
+        atm.topUp(1, 0, 3);
         atm.atmInfo();
-        atm.withdraw();
+
+        int[] testPositive1 = atm.withdraw();
+        int[] testNegative1 = atm.withdraw();
+
     }
 }
